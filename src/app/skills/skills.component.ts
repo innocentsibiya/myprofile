64226db7f58b6533145a5c7b8194248f9cd9b5b6
Innocent Sibiya <innoh.cbia@gmail.com>
2025-07-26 13:29:51 +0200

    skills and reference

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+interface SkillItem {
+  title: string;
+  names: string;
+}
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.less']
 })
 export class SkillsComponent {
-
+  skillsList: SkillItem[] = [
+    {
+      title: 'Languages & Frameworks',
+      names: 'C#, .NET, Angular, Typescript, HTML/CSS, Node.js, Bootstrap'
+    },
+    {
+      title: 'Databases',
+      names: 'SQL Server, PostgreSQL, Oracle SQL, PLSQL, MongoDB'
+    },
+    {
+      title: 'Tools & Platforms',
+      names: 'Git, GitHub, Azure DevOps, Postman, IIS, BI Publisher, Oracle Forms, JSON, YAML, XML'
+    },
+    {
+      title: 'DevOps',
+      names: 'CI/CD, Jenkins, Azure DevOps Pipelines'
+    }
+  ];
 }
