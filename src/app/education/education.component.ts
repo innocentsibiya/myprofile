0bdd8f0a5aab1d2a9b7369971caea13e9f6e46d3
Innocent Sibiya <innoh.cbia@gmail.com>
2025-07-26 13:15:42 +0200

    education

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface EducationItem {
+  title: string;
+  duration: string;
+  name?: string;
+}
+
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.less']
 })
 export class EducationComponent {
-
+   educationList: EducationItem[] = [
+    {
+      title: 'BCom Honours in Information Systems & Technology',
+      duration: '2019',
+      name: 'University of KwaZulu-Natal'
+    },
+        {
+      title: 'BSc in Computer Science and Information Technology',
+      duration: '2016 - 2018'
+    },
+    {
+      title: 'Matriculation',
+      duration: '2012 - 2015',
+      name:'Aquadene Secondary School'
+    }
+  ];
 }
